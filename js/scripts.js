304d9027ed7feb20df4fce71d694e16aff3a1f11
Ben Furdui and Garrett Boggs <garrett@>
2016-08-10 15:10:49 -0700

    add basic features to survey

---
+++ js/scripts.js
@@ -0,0 +1,20 @@
+$(document).ready(function(){
+  $("#info form").submit(function(event) {
+    var userName = $("input#userName").val();
+    var gender = $("input:radio[name=gender]:checked").val();
+
+    var band = $("input:radio[name=band]:checked").val();
+    var favoriteColor = $("#color").val();
+
+    var bev = $("#beverage").val();
+
+     $(".name").text(userName);
+     $(".gender").text(gender);
+     $(".band").text(band);
+     $(".beverage").text(bev);
+
+     $("body").css("background-color",favoriteColor);
+
+     event.preventDefault();
+  });
+});
